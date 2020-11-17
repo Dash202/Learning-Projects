74ab7d8937deaf0fbae566513856963791fff1cb
Dash202 <54924368+Dash202@users.noreply.github.com>
2020-11-17 11:55:25 -0500

    Add files via upload

--- pcap_diff_checker.py
+++ pcap_diff_checker.py
@@ -3,10 +3,15 @@ pcap = pyshark.FileCapture('Test_Capture.pcapng')
 
 for pkt in pcap:
 	try:
-		print(pkt['ip'].src)
+		src = pkt['ip'].src
+		dst = pkt['ip'].dst
+		ip_id = pkt['ip'].id
+		udp_port = pkt['udp'].srcport
+		#print("Source:", src, "Destination:", dst)
+		#print(ip_id)
+		print(udp_port)
 	except:
 		continue
-
 
 #print(pcap)
 
